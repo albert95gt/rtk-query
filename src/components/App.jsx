a6871664a5cb2875dbefe21443c37a72d896b037
albert95gt <albitakacs@gmail.com>
2022-04-19 19:30:20 +0200

    add async rtk operation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,68 @@
+import { useState } from 'react';
+import {
+  useGetContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} from '../redux/contactsApi';
+
 export const App = () => {
-  return <div>React homework template</div>;
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const { data = [], isLoading } = useGetContactsQuery();
+  const [addContact, { isError }] = useAddContactMutation();
+  const [deleteContact] = useDeleteContactMutation();
+
+  const handleAddContact = async e => {
+    e.preventDefault();
+    await addContact({ name, phone });
+    setName('');
+    setPhone('');
+  };
+
+  const handleDeleteContact = async id => {
+    await deleteContact(id);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleAddContact}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </label>
+        <label>
+          Phone:
+          <input
+            type="text"
+            name="phone"
+            value={phone}
+            onChange={e => setPhone(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+      {isLoading && <div>Loading...</div>}
+      {data && (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>
+              <p>Name: {item.name}</p>
+              <p>Phone: {item.phone}</p>
+              <button
+                type="button"
+                onClick={() => handleDeleteContact(item.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
